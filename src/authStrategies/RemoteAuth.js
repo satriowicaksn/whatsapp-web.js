b14c938a04709ed2f39ca028d51b441418b48d94
Satrio Putro Wicaksono <90515555+satriowicaksn@users.noreply.github.com>
2022-08-23 00:22:08 +0700

    Update RemoteAuth.js

--- src/authStrategies/RemoteAuth.js
+++ src/authStrategies/RemoteAuth.js
@@ -95,7 +95,7 @@ class RemoteAuth extends BaseAuthStrategy {
         var self = this;
         this.backupSync = setInterval(async function () {
             await self.storeRemoteSession({saveZip: true});
-            console.log(`[BACKUP] running ..`);
+            console.log('[BACKUP] running ..');
         }, this.backupSyncIntervalMs);
     }
 
